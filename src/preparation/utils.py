d97dfcb5dad0df42fe18259c5333e358b3a92cea
aniciya777 <kisaost777@gmail.com>
2025-06-03 18:08:39 +0500

    Add lib

---
+++ src/preparation/utils.py
@@ -0,0 +1,13 @@
+from pydub import AudioSegment  # type: ignore 
+
+
+# Функция для конвертации ogg в wav
+def convert_ogg_to_wav(input_file: str, output_file: str) -> None:
+    try:
+        # Загружаем ogg файл
+        audio = AudioSegment.from_file(input_file, format="ogg")
+
+        # Сохраняем как wav
+        audio.export(output_file, format="wav")
+    except Exception as e:
+        print(f"convert_ogg_to_wav: Ошибка при конвертации: {e}")
